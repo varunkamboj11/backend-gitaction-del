2696e3d61762bf2578e9ac630eb1d8d333d4c3fb
varunkamboj11 <kambojvarun1111@gmail.com>
2020-10-14 12:12:19 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "multiple_ec2_instances" {
+  ami = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "demo-ec2"
+  }
+}
+
+terraform {
+    backend "s3" {
+    bucket = "day0day01"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "branch_name" {
+  description = "The name of the branch that's being deployed"
+}
+
+####
+######
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "ami" {
+  default = "ami-0947d2ba12ee1ff75"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
